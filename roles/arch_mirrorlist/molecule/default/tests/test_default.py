8320310b1ef243019c8d61b42e99f7e03dcbc574
Dan Nixon <dan@dan-nixon.com>
2021-06-14 14:17:19 +0100

    Correct variable name

--- roles/arch_mirrorlist/molecule/default/tests/test_default.py
+++ roles/arch_mirrorlist/molecule/default/tests/test_default.py
@@ -4,20 +4,20 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-mirrotlist_filename = "/etc/pacman.d/mirrorlist"
+mirrorlist_filename = "/etc/pacman.d/mirrorlist"
 
 
 def test_file_exists(host):
-    assert host.file(mirrotlist_filename).is_file
+    assert host.file(mirrorlist_filename).is_file
 
 
 def test_countries(host):
-    f = host.file(mirrotlist_filename)
+    f = host.file(mirrorlist_filename)
     assert f.contains("## United Kingdom")
     assert f.contains("## Germany")
 
 
 def test_protocols(host):
-    f = host.file(mirrotlist_filename)
+    f = host.file(mirrorlist_filename)
     assert f.contains("https://")
     assert not f.contains("http://")

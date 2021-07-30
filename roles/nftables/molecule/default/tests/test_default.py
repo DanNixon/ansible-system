0787146c82f72bac7c73d6d759a174623f6b3c83
Dan Nixon <dan@dan-nixon.com>
2021-07-30 21:02:33 +0100

    Add flush ruleset to generated nftables config

--- roles/nftables/molecule/default/tests/test_default.py
+++ roles/nftables/molecule/default/tests/test_default.py
@@ -8,4 +8,4 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_file(host):
     f = host.file("/etc/nftables.conf")
     assert f.is_file
-    assert f.md5sum == "d097edb5f414dc8c07c9c02fc6b4c97e"
+    assert f.md5sum == "273eb819684461ece572caec8a0772b0"
